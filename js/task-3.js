2743633bba0cc8b148e539524aaf4d62c42514d8
allaponomarenko <alla.deyneka03@gmail.com>
2024-04-26 10:06:09 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('name-input');
+    const output = document.getElementById('name-output');
+
+    input.addEventListener('input', () => {
+        const trimmedInput = input.value.trim();
+        if (trimmedInput) {
+            output.textContent = trimmedInput;
+        } else {
+            output.textContent = 'Anonymous';
+        }
+    });
+});
