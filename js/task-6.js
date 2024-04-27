0670c277e4f724d8f002d8f3a96cbc3c62e4122a
allaponomarenko <alla.deyneka03@gmail.com>
2024-04-27 16:50:16 +0200

    task-6(total)

--- js/task-6.js
+++ js/task-6.js
@@ -1,43 +1,47 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const createButton = document.querySelector('[data-create]');
+  const destroyButton = document.querySelector('[data-destroy]');
+  const input = document.querySelector('#controls input[type="number"]');
+  const boxesContainer = document.getElementById('boxes');
+
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
   }
 
   function createBoxes() {
-    const input = document.querySelector('#controls input[type="number"]');
     const amount = Number(input.value);
     if (amount < 1 || amount > 100) {
-      console.warning('The number of boxes should be between 1 and 100. Input was:', amount);
+      console.warn('The number of boxes should be between 1 and 100. Input was:', amount);
       return;
     }
 
     destroyBoxes();
-    const boxesContainer = document.getElementById('boxes');
     let currentSize = 30;
+    let markup = '';
 
     for (let i = 0; i < amount; i++) {
-      const box = document.createElement('div');
-      box.style.width = `${currentSize}px`;
-      box.style.height = `${currentSize}px`;
-      box.style.backgroundColor = getRandomHexColor();
-      boxesContainer.appendChild(box);
+      markup += `<div style="width: ${currentSize}px; height: ${currentSize}px; background-color: ${getRandomHexColor()};"></div>`;
       currentSize += 10;
     }
 
+    boxesContainer.innerHTML = markup;
     input.value = '';
   }
 
   function destroyBoxes() {
-    const boxesContainer = document.getElementById('boxes');
     boxesContainer.innerHTML = '';
   }
-
-  const createButton = document.querySelector('[data-create]');
-  const destroyButton = document.querySelector('[data-destroy]');
 
   createButton.addEventListener('click', createBoxes);
   destroyButton.addEventListener('click', destroyBoxes);
 });
+
+
+
+
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
